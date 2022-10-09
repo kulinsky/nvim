c428849e3f4423a449bab17e50e4e433a3d8ef55
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2022-10-10 00:50:23 +0500

    add telescope keys

--- lua/remap.lua
+++ lua/remap.lua
@@ -4,3 +4,11 @@ local inoremap = require("keymap").inoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 inoremap("jk", "<ESC>")
+
+
+local telescope_b = require("telescope.builtin")
+nnoremap("ff", telescope_b.find_files)
+nnoremap("fg", telescope_b.live_grep)
+nnoremap("fb", telescope_b.buffers)
+nnoremap("fh", telescope_b.help_tags)
+
