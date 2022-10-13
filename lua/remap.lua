be2708c4ec493d8d00859a779a9e3002ca233862
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2022-10-13 17:35:14 +0500

    remap

--- lua/remap.lua
+++ lua/remap.lua
@@ -12,3 +12,19 @@ nnoremap("fg", telescope_b.live_grep)
 nnoremap("fb", telescope_b.buffers)
 nnoremap("fh", telescope_b.help_tags)
 
+nnoremap("K", vim.lsp.buf.hover)
+nnoremap("<C-k>", vim.lsp.buf.signature_help)
+nnoremap("gD", vim.lsp.buf.declaration)
+nnoremap("gd", vim.lsp.buf.definition)
+nnoremap("gr", vim.lsp.buf.references)
+nnoremap("gi", vim.lsp.buf.implementation)
+nnoremap("[d",
+    function() 
+        vim.diagnostic.goto_prev(border_options)
+    end
+)
+nnoremap("]d",
+    function()
+        vim.diagnostic.goto_next(border_options)
+    end
+)
